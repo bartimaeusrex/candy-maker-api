946f2ce7d821f1c9eacbe937f5308fc008a80e13
bartimaeusrex <mrpatrickreinhart@gmail.com>
2020-05-14 18:15:08 -0400

    Everything working

--- migrations/20200514011330-initial-table-creation.js
+++ migrations/20200514011330-initial-table-creation.js
@@ -1,7 +1,5 @@
-'use strict';
-
 module.exports = {
-  up: (queryInterface, Sequelize) => {
+  up: async (queryInterface, Sequelize) => {
     /*
       Add altering commands here.
       Return a promise to correctly handle asynchronicity.
@@ -9,9 +7,27 @@ module.exports = {
       Example:
       return queryInterface.createTable('users', { id: Sequelize.INTEGER });
     */
+    await queryInterface.createTable('manufacturers', {
+      id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
+      name: { type: Sequelize.STRING, allowNull: false },
+      country: { type: Sequelize.STRING },
+      createdAt: { type: Sequelize.DATE, defaultValue: Sequelize.literal('CURRENT_TIMESTAMP') },
+      updatedAt: { type: Sequelize.DATE, defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP') },
+      deletedAt: { type: Sequelize.DATE },
+    })
+
+    return queryInterface.createTable('products', {
+      id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
+      name: { type: Sequelize.STRING, allowNull: false },
+      yearIntroduced: { type: Sequelize.DATEONLY },
+      manufacturerId: { type: Sequelize.INTEGER, references: { model: 'manufacturers', key: 'id' } },
+      createdAt: { type: Sequelize.DATE, defaultValue: Sequelize.literal('CURRENT_TIMESTAMP') },
+      updatedAt: { type: Sequelize.DATE, defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP') },
+      deletedAt: { type: Sequelize.DATE },
+    })
   },
 
-  down: (queryInterface, Sequelize) => {
+  down: async (queryInterface) => {
     /*
       Add reverting commands here.
       Return a promise to correctly handle asynchronicity.
@@ -19,5 +35,8 @@ module.exports = {
       Example:
       return queryInterface.dropTable('users');
     */
+    await queryInterface.dropTable('products')
+
+    return queryInterface.dropTable('manufacturers')
   }
-};
+}
